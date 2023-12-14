7a0dae4ef193850f478eb144c61b92c88628ef06
Alefe Rocha <alefe.rtavares@gmail.com>
2023-12-14 19:31:18 -0300

    build(comments): css of the comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.commentAvatar} src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+           
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriela Pereira</strong>
+                            <time title= "21 de Outubro às 11h27" dateTime="2023-10-21 11:27:31">Publicado há 1h</time>
+                        </div>
+                        <button title="Delete comment">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Congrats, Alefe! Nice project 🔥</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Like <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
